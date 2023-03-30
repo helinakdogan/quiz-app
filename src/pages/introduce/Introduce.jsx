9cea672bda88cbfab3fbf0bc0c2fb2d826aa7400
Helin Akdoğan <akdoganhelin@gmail.com>
2023-03-31 02:57:17 +0300

    31.03.2023

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -11,9 +11,9 @@ const difficulty = ["easy", "medium", "hard"];
   return (
     <div className='introduce'>
       <div className="introduce-container">
-        <img src="https://images.squarespace-cdn.com/content/v1/5402e23ce4b0a7034afad3a2/1556115865242-8PPL2J1DIGRL9ZRU8K9P/Quiz%2Btime%2Bgin%2Bfact%2Bfiction.png?format=2500w" alt="" width={600} />
+        <img className="image" src="https://images.squarespace-cdn.com/content/v1/5402e23ce4b0a7034afad3a2/1556115865242-8PPL2J1DIGRL9ZRU8K9P/Quiz%2Btime%2Bgin%2Bfact%2Bfiction.png?format=2500w" alt="" width={600} />
       <Dropdown/>
-      
+      <div className='introduce-btn'>Quize başla</div>
       </div>
     </div>
   )
