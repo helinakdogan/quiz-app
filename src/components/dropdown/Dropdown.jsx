05f93f366d6f7d3ca29f1712a0d6806539378315
Helin Akdoğan <akdoganhelin@gmail.com>
2023-04-17 23:34:55 +0300

    17.04.2023

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Dropdown.css'
 
+
+// First it gets some data from Introduce page
 const Dropdown = ({ data, setDifficultyChange }) => {
   return (
     <div className='dropdown'>
+      {/* When user selects an option, it will become current option */}
       <select onChange={e => setDifficultyChange(e.target.value)}  name="" id="">
         {
+          //to show array on screen
           data.map((dt, i) => (
             <option value={dt}> {dt} </option>
           ))
