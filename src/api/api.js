49a468673201b86791215c6322eb8d7f4bc204ff
Helin Akdoğan <akdoganhelin@gmail.com>
2023-04-21 18:12:52 +0300

    21.04.2023

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 
+// 1 -> get correct and incorrect answers from url
+// 2 -> shuffle them random
+
+
 const shuffleArray = (array) => {
-    //get arrays items seperately and sort them
-    //put them in random order
+    // 2. Get arrays items seperately and sort them
+    // 2. Put them in random order
     return [...array].sort(() => Math.random() - 0.5)
 }
 
@@ -10,10 +14,9 @@ export const fetchQuizData = async (difficulty, amount) => {
     const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(url)).json();
     return data.results.map((dt) => ({
-
-        //get all responses
+        // 1. First, get all responses
         ...dt,
-        //get all incorrect answers and one correct answer but correct one shouldn't always be D. so that we use function
+        // 1. Second, get all incorrect answers and one correct answer but correct one shouldn't always be D. so that we use function
         answers: shuffleArray([...dt.incorrect_answers, dt.correct_answer])
     }))
 }
